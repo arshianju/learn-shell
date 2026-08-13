d019afe5471bad4a03b4ccb128fd3f56fd711e33
asifa <arshiyaanjum0103@gmail.com>
2026-08-14 00:21:20 +0530

    print file exec

---
+++ print.sh
@@ -0,0 +1,14 @@
+echo -e \\e[31mHello World\\e[0m
+echo -e \\e[32mHello World\\e[0m
+echo -e \\e[33mHello World\\e[0m
+echo -e \\e[34mHello World\\e[0m
+echo -e \\e[35mHello World\\e[0m
+echo -e \\e[36mHello World\\e[0m
+
+
+echo -e \\e[41mHello World\\e[0m
+echo -e \\e[42mHello World\\e[0m
+echo -e \\e[43mHello World\\e[0m
+echo -e \\e[44mHello World\\e[0m
+echo -e \\e[45mHello World\\e[0m
+echo -e \\e[46mHello World\\e[0m
