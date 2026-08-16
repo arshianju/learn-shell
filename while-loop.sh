f260d9afbb68afba2e00193022cf2e9ab5995bd3
asifa <arshiyaanjum0103@gmail.com>
2026-08-16 15:05:45 +0530

    whileloop file exec

---
+++ while-loop.sh
@@ -0,0 +1,18 @@
+
+X=5
+while [ $X -gt 0 ]; do
+  echo "Value: $X"
+  X=$((X-1))
+  sleep 2
+done
+
+nc -w 2 -z localhost 3000
+while [ $? -ne 0 ]; do
+  sleep 2
+  echo Trying again after 30 seconds...
+  nc -w 2 -z localhost 3000
+done
+
+
+
+echo Service on 3000 is started successfully.
